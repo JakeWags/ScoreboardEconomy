c781f88d90734ee7f205f57fe5e030aeddd04352
Jake Wagoenr <jakewagoneredu@gmail.com>
2021-12-14 16:44:10 -0700

    Cleaner imports

--- src/main/java/com/jakew/sceco/ScoreboardEconomy.java
+++ src/main/java/com/jakew/sceco/ScoreboardEconomy.java
@@ -1,30 +1,14 @@
 package com.jakew.sceco;
 
-import static com.mojang.brigadier.arguments.IntegerArgumentType.getInteger;
-import static com.mojang.brigadier.arguments.IntegerArgumentType.integer;
-import static com.mojang.brigadier.arguments.StringArgumentType.*;
-import static net.minecraft.server.command.CommandManager.literal;
-import static net.minecraft.server.command.CommandManager.argument;
-
 import com.jakew.sceco.commands.BalanceCommand;
 import com.jakew.sceco.commands.CatalogCommand;
 import com.jakew.sceco.commands.PayCommand;
 import com.jakew.sceco.commands.SellCommand;
 import com.jakew.sceco.shop.Catalog;
-import com.jakew.sceco.shop.ShopItem;
-import com.jakew.sceco.util.CatalogItemNotFoundException;
-import com.mojang.brigadier.tree.LiteralCommandNode;
+
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
-import net.minecraft.item.ItemStack;
 import net.minecraft.scoreboard.Scoreboard;
-import net.minecraft.scoreboard.ScoreboardObjective;
-import net.minecraft.server.command.CommandManager;
-import net.minecraft.server.command.ServerCommandSource;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.text.Text;
-
-import java.util.Collection;
 
 public class ScoreboardEconomy implements ModInitializer {
     public static final String MOD_ID = "sceco";
